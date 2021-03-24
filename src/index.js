f35fef77e456c514e1f6a6adf72a3513e32dde64
Ben Haderle <benhaderle@gmail.com>
2021-03-24 09:02:37 -0700

    fixing authentication

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
+Amplify.configure(config);
+
 // //const Jimp = require('jimp');
 // import Jimp from 'jimp';
 // const fs = require('fs')
 // const path = require('path')
 // const {spawn} = require('child_process')
 
-// Amplify.configure(config);
+// 
 
 // console.log("test")
 // console.log(__filename)
